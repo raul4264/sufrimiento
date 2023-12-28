8c9d33d0a29e71d4345fb93bf4ba049de0ee9208
Raul novas <raulnovas4@gmail.com>
2023-12-27 21:53:11 -0300

    reparaciones varias de fotos commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import styles from "./HomeStyles.module.css";
 function Home() {
   return (
     <div className={styles.home}>
-      <span>Sneaker</span>
+      <span>Nike</span>
 
       <h1 className={styles.h1}>Store - by React</h1>
     </div>
