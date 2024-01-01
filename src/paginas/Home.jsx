91cb85f7df76596607628d50426cbba5052a8d94
Raul novas <raulnovas4@gmail.com>
2024-01-01 14:28:41 -0300

    home retoques esteticos

--- src/paginas/Home.jsx
+++ src/paginas/Home.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Footer from "./Footer";
 import "./Styles.css";
 
@@ -9,6 +8,7 @@ function Home() {
         src="https://images.unsplash.com/photo-1608541737042-87a12275d313?q=80&w=2061&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
       ></img>
+      <Footer />
       <div className="texto-encima">
         <h1>
           123 Calle Ficticia, Ciudad Imaginaria, Provincia de la Imaginación,
@@ -16,8 +16,6 @@ function Home() {
         </h1>
       </div>
       <div className="centrado">Nike.Shop</div>
-
-      <Footer />
     </div>
   );
 }
